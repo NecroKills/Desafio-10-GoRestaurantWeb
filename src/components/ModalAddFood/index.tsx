862ad0c0963cfffc0d6a5c9045ce67b1eff1a07d
Maycon <maycondasilvamoreira@hotmail.com>
2020-06-10 20:26:02 -0300

    Adicionado para abrir o modal e fechar modal

--- src/components/ModalAddFood/index.tsx
+++ src/components/ModalAddFood/index.tsx
@@ -38,6 +38,7 @@ const ModalAddFood: React.FC<IModalProps> = ({
   const handleSubmit = useCallback(
     async (data: ICreateFoodData) => {
       handleAddFood(data);
+      setIsOpen();
     },
     [handleAddFood, setIsOpen],
   );
