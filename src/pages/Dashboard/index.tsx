1a71d56fbd881777ed01b84bf8cdc4574c3a9bdd
Maycon <maycondasilvamoreira@hotmail.com>
2020-06-10 20:17:01 -0300

    Foi desenvolvido para deletar um prato de comida.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -27,7 +27,9 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // TODO LOAD FOODS
+      const response = await api.get('/foods');
+
+      setFoods(response.data);
     }
 
     loadFoods();
@@ -37,7 +39,8 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     try {
-      // TODO ADD A NEW FOOD PLATE TO THE API
+      const response = await api.post('/foods', { ...food, available: true });
+      setFoods(state => [...state, response.data]);
     } catch (err) {
       console.log(err);
     }
@@ -50,7 +53,9 @@ const Dashboard: React.FC = () => {
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`/foods/${id}`);
+
+    setFoods(state => state.filter(stateFood => stateFood.id !== id));
   }
 
   function toggleModal(): void {
